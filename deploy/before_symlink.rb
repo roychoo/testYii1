8f685db003bf727b98f77b7051ae6e88df73674d
Roy Choo <roychoo@Roys-MacBook-Air.local>
2013-12-06 00:02:18 +0800

    adding some chef hooks

---
+++ deploy/before_symlink.rb
@@ -0,0 +1,19 @@
+app_current = ::File.expand_path("#{release_path}/../../current")
+vendor_dir  = "#{app_current}/vendor"
+
+deploy_user  = "www-data"
+deploy_group = "www-data"
+
+release_vendor = "#{release_path}/vendor"
+
+::FileUtils.cp_r vendor_dir, release_vendor if ::File.exists?(vendor_dir)
+::FileUtils.chown_R deploy_user, deploy_group, release_vendor if ::File.exists?(release_vendor)
+
+composer_command = "/usr/local/bin/php"
+composer_command << " #{release_path}/composer.phar"
+composer_command << " --no-dev"
+composer_command << " --prefer-source"
+composer_command << " --optimize-autoloader"
+composer_command << " install"
+
+run "cd #{release_path} && #{composer_command}"
